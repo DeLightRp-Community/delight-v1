0a4ddf4a247018c07e6f2f32d73c03e254d338c7
Server <hkarimi561@gmail.com>
2022-05-24 03:13:21 -0400

    fix elevator

--- [qb]/re2-elevator/config.lua
+++ [qb]/re2-elevator/config.lua
@@ -3,19 +3,21 @@ Config = {}
 Config.locations = {
     ["Pilbox"] = {
         [1] = {
-            ["Name"] = "Philbox First Floor",
-            ["teleport"] = vector4(0,0,0,0),
-            ["button"] = vector4(0,0,0,0)
+            ["name"] = "Philbox RoofTop",
+            ["teleport"] = vector4(338.53, -583.9, 74.16, 252.7),
+            ["button"] = vector4(338.53, -583.9, 74.16, 252.7)
         },
         [2] = {
-            ["Name"] = "Philbox Parking",
-            ["teleport"] = vector4(0,0,0,0),
-            ["button"] = vector4(0,0,0,0)
+            ["name"] = "Philbox First Floor",
+            ["teleport"] = vector4(332.39, -595.77, 43.28, 70.94),
+            ["button"] = vector4(331.74, -596.9, 43.28, 245.58)
         },
         [3] = {
-            ["Name"] = "Philbox RoofTop",
-            ["teleport"] = vector4(0,0,0,0),
-            ["button"] = vector4(0,0,0,0)
+            ["name"] = "Philbox Parking",
+            ["teleport"] = vector4(344.28, -586.2, 28.8, 244.59),
+            ["button"] = vector4(345.05, -584.85, 28.8, 73.66)
         },
+        
     }
 }
+
