24f3c9ed901c2b7da31f3ff6dc46997cf33a1223
Hamid <hkarimi561@gmail.com>
2022-07-29 23:55:54 +0430

    add digital den doors

--- [qb]/qb-doorlock/configs/digitalden.lua
+++ [qb]/qb-doorlock/configs/digitalden.lua
@@ -1,29 +1,87 @@
 
 
--- digitalden1 created by ubadboy
-Config.DoorList['digitalden-digitalden1'] = {
+-- 1 created by Mosayeb
+Config.DoorList['digitalden-1'] = {
     distance = 2,
-    objYaw = 74.84864807129,
+    objYaw = 254.85559082032,
+    doorRate = 1.0,
     locked = true,
-    objCoords = vec3(1137.211670, -470.011170, 66.941764),
-    objName = 2065277225,
+    objName = 1181020301,
+    doorLabel = 'front door',
+    authorizedJobs = { ['digitalden'] = 0 },
+    objCoords = vec3(1137.338746, -470.038696, 66.906312),
     doorType = 'door',
     fixText = false,
-    doorRate = 1.0,
-    authorizedJobs = { ['digitalden'] = 0 },
-    doorLabel = 'asle',
 }
 
--- digitalden2 created by ubadboy
-Config.DoorList['digitalden-digitalden2'] = {
+-- 2 created by Mosayeb
+Config.DoorList['digitalden-2'] = {
     distance = 2,
-    objYaw = 74.96957397461,
+    objYaw = 76.124114990234,
+    doorRate = 1.0,
     locked = true,
-    objCoords = vec3(1130.779786, -475.226806, 66.867920),
-    objName = -2023754432,
+    objName = 547719377,
+    doorLabel = 'front desk door',
+    hideLabel = true,
+    authorizedJobs = { ['digitalden'] = 0 },
+    objCoords = vec3(1136.689332, -466.895234, 66.634522),
     doorType = 'door',
     fixText = false,
+}
+
+-- 3 created by Mosayeb
+Config.DoorList['digitalden-3'] = {
+    distance = 2,
+    objYaw = 75.007148742676,
     doorRate = 1.0,
+    locked = true,
+    objName = -1565285813,
+    doorLabel = 'back door',
     authorizedJobs = { ['digitalden'] = 0 },
-    doorLabel = 'posht',
+    objCoords = vec3(1129.294190, -464.659240, 66.641548),
+    doorType = 'door',
+    fixText = false,
+}
+
+-- 4 created by Mosayeb
+Config.DoorList['digitalden-4'] = {
+    distance = 2,
+    objYaw = 74.993217468262,
+    doorRate = 1.0,
+    locked = true,
+    objName = -1503577684,
+    doorLabel = 'balcony door',
+    authorizedJobs = { ['digitalden'] = 0 },
+    objCoords = vec3(1137.303222, -474.208740, 72.118126),
+    doorType = 'door',
+    fixText = false,
+}
+
+-- 5 created by Mosayeb
+Config.DoorList['digitalden-5'] = {
+    distance = 2,
+    objYaw = 76.124122619628,
+    doorRate = 1.0,
+    locked = true,
+    objName = -1714438833,
+    doorLabel = 'kitchen door',
+    authorizedJobs = { ['digitalden'] = 0 },
+    objCoords = vec3(1131.722534, -463.672760, 65.485070),
+    doorType = 'sliding',
+    fixText = false,
+}
+
+-- 6 created by Mosayeb
+Config.DoorList['digitalden-6'] = {
+    distance = 2,
+    doorLabel = 'refregrator door',
+    doorRate = 1.0,
+    doors = {
+        {objName = -1577379202, objYaw = 165.99435424804, objCoords = vec3(1136.401000, -464.168366, 66.509804)},
+        {objName = -1577379202, objYaw = 346.06643676758, objCoords = vec3(1135.493164, -463.911376, 66.509804)}
+    },
+    authorizedJobs = { ['digitalden'] = 0 },
+    locked = true,
+    doorType = 'double',
+    hideLabel = true,
 }
